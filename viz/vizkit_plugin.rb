5e304e1ec73ab6e5afc5d35e40c7905d58c86bf3
Sascha Arnold <sascha.arnold@dfki.de>
2016-08-08 16:35:21 +0200

    viz: alias typenames are now supported

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,6 +1,6 @@
 Vizkit::UiLoader.register_3d_plugin('OcTreeVisualization', 'octomap', 'OcTreeVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/boost/shared_ptr</octomap/AbstractOcTree>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/envire/core/SpatioTemporal</boost/shared_ptr</octomap/AbstractOcTree>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/octomap/AbstractOcTreePtr", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/envire/core/SpatioTemporal</octomap/AbstractOcTreePtr>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
