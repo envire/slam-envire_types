90bf8ec4c55f98424a73861c7e19c2e6d4a8a2dd
Sascha Arnold <sascha.arnold@dfki.de>
2018-09-14 13:34:21 +0200

    fixed usage of getVisualizationFrames
    The setter already evaluates if the frame is available

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -26,8 +26,6 @@
 Vizkit::UiLoader.register_3d_plugin('OcTreeVisualization', 'octomap', 'OcTreeVisualization')
 Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/octomap/AbstractOcTreePtr", :updateData )
 Vizkit::UiLoader.register_3d_plugin_for('OcTreeVisualization', "/envire/core/SpatioTemporal</octomap/AbstractOcTreePtr>") do |plugin,sample,_|
-    if plugin.getVisualizationFrames.include? sample.frame_id
-        plugin.setVisualizationFrame(sample.frame_id)
-    end
+    plugin.setVisualizationFrame(sample.frame_id)
     plugin.updateData(sample.data)
 end
